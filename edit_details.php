86da2b536f515b10fa4f86ab92dc13575817f0e7
atmni5 <aaron.tumini@griffithuni.edu.au>
2017-05-12 21:11:33 +1000

    Edit details null age
    - Added functionality for when age is 0 to enter NULL as age

--- edit_details.php
+++ edit_details.php
@@ -6,12 +6,19 @@
 
     $name = $_POST['name'];
     $age = $_POST['age'];
+    if ($age != 0){
+        $age = '\''.$_POST['age'].'\'';   
+    } else {
+        $age = "NULL";
+    }
+
     $password = $_POST['password'];
     $sql;
+
     if ($password != ""){
-        $sql = "Update tbl_Users set Name = '$name', Age = '$age', Password = '$password' where U_ID = '". $_SESSION['U_ID'] ."'";
+        $sql = "Update tbl_Users set Name = '$name', Age = $age, Password = '$password' where U_ID = '". $_SESSION['U_ID'] ."'";
     } else {
-        $sql = "Update tbl_Users set Name = '$name', Age = '$age' where U_ID = '". $_SESSION['U_ID'] ."'";
+        $sql = "Update tbl_Users set Name = '$name', Age = $age where U_ID = '". $_SESSION['U_ID'] ."'";
     }
 
     $result = mysqli_query($con, $sql);
